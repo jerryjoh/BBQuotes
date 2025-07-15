7f5fff7835a36e7f7f5edd2a9b1e9edb9bf26bbc
Jerry Johnson <jerryjoh@yahoo.com>
2025-07-15 16:46:48 -0400

    Initial Commit

---
+++ BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Jerry Johnson on 7/15/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
